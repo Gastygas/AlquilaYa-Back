6f2f1b2f67b348c4be8738589421f65133c4046e
Gastygas <gastonrodrigogonzalez55@gmail.com>
2024-10-17 15:04:24 -0300

    property entity

--- back/src/entities/property.entity.ts
+++ back/src/entities/property.entity.ts
@@ -1,6 +1,66 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {v4 as uuid} from 'uuid';
 
 @Entity("property")
 export class Property{
+    @PrimaryGeneratedColumn()
+    id:string = uuid();
+
+    @Column({nullable: false})
+    photos:string[];
+
+    @Column()
+    disableDays:Date[];
+
+    @Column({type: 'varchar',nullable: false})
+    adress:string;
+
+    @Column({type: 'varchar',nullable: false})
+    adressUrl:string;
+
+    @Column({type: 'varchar',nullable: false})
+    bill:string;
+
+    @Column({type: 'varchar',nullable: false})
+    country:string;
+
+    @Column({type: 'varchar',nullable: false})
+    city:string;
+
+    @Column({type:'int',nullable: false})
+    price:number;
+
+    @Column({type:'int',nullable: false})
+    capacity:number;
+
+    @Column({type:'int',nullable: false})
+    bedrooms:number;
+
+    @Column({type:'int',nullable: false})
+    bathrooms:number;
+
+    @Column({type:'boolean',nullable: false})
+    wifi:boolean;
     
+    @Column({type:'boolean',nullable: false})
+    petFriendly:boolean;
+
+    @Column({type:'boolean',nullable: false})
+    airConditioning: boolean;
+
+    @Column({type:'boolean',nullable: false})
+    heating:boolean;
+
+    @Column({type:'boolean',nullable: false})
+    pool:boolean;
+
+    @Column({type:'boolean',nullable: false})
+    parking:boolean;
+
+    @Column({type: 'varchar'})
+    description:string;
+
+    @Column({type: 'varchar'})
+    propertyStatus:string;
+
 }
