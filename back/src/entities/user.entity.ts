96995ae4b689804f48f7e45daaf13dff84a051fb
benjadelcampo15 <benjadelcampo15@gmail.com>
2024-10-17 14:41:37 -0300

    entidad user creada

---
+++ back/src/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string = uuid();
+
+  @Column({ nullable: false, type: 'varchar', length: 30 })
+  name: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 30 })
+  surname: string;
+
+  @Column({ unique: true, nullable: false, type: 'varchar', length: 50 })
+  email: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  password: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  dni: string;
+
+  @Column({ default: false, nullable: true })
+  isAdmin: boolean;
+
+  @Column({ nullable: false, type: 'varchar', length: 30 })
+  country: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 50 })
+  adress: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 30 })
+  phone: string;
+
+    
+}
