d0198c908f5bf3460873f7146426a1df8e009427
Hugo <hugoestebanaguero@gmail.com>
2024-10-18 01:39:31 -0300

    Module Property created. Swagger installed

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PropertyModule } from './modules/property/property.module';
 import typeormConfig from './config/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [typeormConfig] }),
     TypeOrmModule.forRootAsync({
-    inject: [ConfigService],
-    useFactory: (configService: ConfigService) =>
-      configService.get('typeorm'),
-    
-  }),
-],
-  
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeorm'),
+    }),
+    PropertyModule,
+  ],
+
   controllers: [],
   providers: [],
-  
 })
 export class AppModule {}
