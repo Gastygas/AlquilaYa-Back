e0482d0ada497531e6dc42ef4cfc3ea5a560b66f
Gastygas <gastonrodrigogonzalez55@gmail.com>
2024-10-18 22:52:22 -0300

    response to errors

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist:true,
+    exceptionFactory:(err) => {
+      const errors = err.map((error) => {
+        return {property: error.property, constraints: error.constraints}
+      })
+    
+    return new BadRequestException({
+      alert:'ERRORS!, please read carefully',
+      error: errors
+    })
+    }
+  }))
+
   await app.listen(3000);
 }
 bootstrap();
