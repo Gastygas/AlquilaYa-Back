f7fbc1572806467130f83fadbc0dfdbe211375b8
Gastygas <gastonrodrigogonzalez55@gmail.com>
2024-10-19 00:08:31 -0300

    auth

--- back/src/modules/auth/auth.controller.ts
+++ back/src/modules/auth/auth.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { SignUpDto } from "./dto/signUp.dto";
 import { SignInDto } from "./dto/signIn.dto";
+import { ApiTags } from "@nestjs/swagger";
 
+@ApiTags('auth')
 @Controller("auth")
 export class AuthController{
     constructor(private readonly authService:AuthService){}
