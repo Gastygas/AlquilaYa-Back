2e82dfb82eb6c3cecd97f362a67417c4c53b60f5
Gastygas <gastonrodrigogonzalez55@gmail.com>
2024-10-21 00:23:15 -0300

    roles added

---
+++ back/src/guards/roles.guard.ts
@@ -0,0 +1,25 @@
+import { BadRequestException, CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { Roles } from "src/modules/users/enum/user.roles.enum";
+
+@Injectable()
+export class RolesGuard implements CanActivate{
+    constructor(private readonly reflector:Reflector){}
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+
+        const requiredRoles = this.reflector.getAllAndOverride<Roles[]>('roles',[context.getHandler(),context.getClass()])
+
+        const request = context.switchToHttp().getRequest()
+        const user = request.user
+        const hasRole = () => requiredRoles.some(role => user?.roles?.includes(role))
+
+        const valid = user && user.roles && hasRole() 
+        if(!valid) throw new BadRequestException("You are not admin")
+        
+        return valid
+
+    }
+    
+}
