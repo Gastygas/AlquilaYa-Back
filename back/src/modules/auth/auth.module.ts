345f4a38d3582e4e41d9ad905510b33eb763d9c4
benjadelcampo15 <benjadelcampo15@gmail.com>
2024-10-22 21:11:02 -0300

    actualizacion auth

--- back/src/modules/auth/auth.module.ts
+++ back/src/modules/auth/auth.module.ts
@@ -4,10 +4,11 @@ import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { UsersRepository } from '../users/users.repository';
+import { GoogleStrategy } from 'src/google.strategy/google.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService, UsersRepository],
+  providers: [AuthService, UsersRepository, GoogleStrategy],
 })
 export class AuthModule {}
