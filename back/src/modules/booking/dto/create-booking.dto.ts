2e21909129236e65bc10c5be6df03349a97c85dc
Gastygas <gastonrodrigogonzalez55@gmail.com>
2024-10-24 15:54:51 -0300

    apiproperty

--- back/src/modules/booking/dto/create-booking.dto.ts
+++ back/src/modules/booking/dto/create-booking.dto.ts
@@ -1,7 +1,10 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateBookingDto {
-    
+    @ApiProperty({example:"eaa0db31-08f5-45f0-aa49-6badcbef7468"})
     propertyId: string;
 
+    @ApiProperty({example:Date()})
     dateStart: Date;
 
     dateEnd: Date;
