c064ed7ca6753428b179209666e8c8325e554d16
Gastygas <gastonrodrigogonzalez55@gmail.com>
2024-10-24 13:39:27 -0300

    bookingModule

--- back/src/modules/booking/booking.module.ts
+++ back/src/modules/booking/booking.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Booking } from 'src/entities/booking.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Booking])],
   controllers: [BookingController],
   providers: [BookingService],
 })
--- back/src/modules/booking/booking.repository.ts
+++ back/src/modules/booking/booking.repository.ts
@@ -1,10 +1,34 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Booking } from "src/entities/booking.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class BookingRepository{
-    constructor(){}
+    constructor(
+        @InjectRepository(Booking)
+        private readonly bookingRepository:Repository<Booking>
+    ){}
 
-    async getBookings(){
-        return "Get All Bookings";
+    async getBookings(page=1,limit=5){
+        const findBookings = await this.bookingRepository.find({
+            skip: (page - 1) * limit,
+            take:limit,
+            relations:{
+                user:true,
+                property:true,
+            }
+        })
+
+        const bookings = findBookings.map((booking) => {
+            const {user,property,...restBooking} = booking
+            return {
+                ...restBooking,
+                property: {id: property.id},
+                user: {id: user.id}
+            }
+        })
+
+        return bookings;
     }
 }
