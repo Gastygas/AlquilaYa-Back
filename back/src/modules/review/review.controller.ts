9d10e92f54207f560dcdada343219237aeb1f409
Hugo <hugoestebanaguero@gmail.com>
2024-10-30 21:03:44 -0300

    create-property.dto modified

--- back/src/modules/review/review.controller.ts
+++ back/src/modules/review/review.controller.ts
@@ -1,16 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('review')
 @Controller('review')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
-  @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewService.create(createReviewDto);
-  }
+  //-----------------------------------------------------------------------------------------
+  //------Obtener reseñas de una propiedad específica (GET /properties/:propertyId/reviews)
+  //------Obtener reseñas de un usuario específico (GET /users/:userId/reviews)
+  //-----------------------------------------------------------------------------------------
 
   @Get()
   findAll() {
@@ -22,10 +32,27 @@ export class ReviewController {
     return this.reviewService.findOne(+id);
   }
 
+  //-----------------------------------------------------------------------------------------
+  //----------- Crear una nueva reseña (POST /reviews)
+  //-----------------------------------------------------------------------------------------
+
+  @Post()
+  create(@Body() createReviewDto: CreateReviewDto) {
+    return this.reviewService.create(createReviewDto);
+  }
+
+  //-----------------------------------------------------------------------------------------
+  //----------- Actualizar una reseña (PUT /reviews/:reviewId)
+  //-----------------------------------------------------------------------------------------
+
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateReviewDto: UpdateReviewDto) {
     return this.reviewService.update(+id, updateReviewDto);
   }
+
+  //-----------------------------------------------------------------------------------------
+  //----------- Eliminar una reseña (DELETE /reviews/:reviewId)
+  //-----------------------------------------------------------------------------------------
 
   @Delete(':id')
   remove(@Param('id') id: string) {
