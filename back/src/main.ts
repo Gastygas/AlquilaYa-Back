e612fa62547e78e5f7a50f247c8f6d8e244546de
Hugo <hugoestebanaguero@gmail.com>
2024-11-06 14:48:28 -0300

    endpoint GET /users/auth0/protected & auth0.config added

--- back/src/main.ts
+++ back/src/main.ts
@@ -2,25 +2,30 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { auth } from 'express-openid-connect';
+import { config as auth0Config } from './config/auth0.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
- //Manejo de errores
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist:true,
-    exceptionFactory:(err) => {
-      const errors = err.map((error) => {
-        return {property: error.property, constraints: error.constraints}
-      })
-    
-    return new BadRequestException({
-      alert:'ERRORS!, please read carefully',
-      error: errors
-    })
-    }
-  }))
-  
+  app.use(auth(auth0Config));
+
+  //Manejo de errores
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      exceptionFactory: (err) => {
+        const errors = err.map((error) => {
+          return { property: error.property, constraints: error.constraints };
+        });
+
+        return new BadRequestException({
+          alert: 'ERRORS!, please read carefully',
+          error: errors,
+        });
+      },
+    }),
+  );
+
   //* Configuración de Swagger
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Proyecto Final - AlquilaYa')
@@ -32,32 +37,31 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
 
-   const corsOptions = {
-     origin: 'http://localhost:3000',
-     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-     credentials: true,
-  }
-  
-   app.enableCors({
+  const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+
+  app.enableCors({
     origin: '*', // Permite cualquier origen
-     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
     credentials: true, // Permite envío de cookies si es necesario
-   });
+  });
 
-  
-   async function bootstrap() {
-      const app = await NestFactory.create(AppModule);
-     app.useGlobalPipes(new ValidationPipe());
-      app.enableCors(); // Permite CORS si es necesario
-      await app.listen(3002);
-   }
-   bootstrap();
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
+    app.enableCors(); // Permite CORS si es necesario
+    await app.listen(3002);
+  }
+  bootstrap();
 
-//   app.enableCors({
-//     origin: 'https://alquilaya.vercel.app',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
-//     credentials: true,  // Permite todas las solicitudes de origen
-// });
+  //   app.enableCors({
+  //     origin: 'https://alquilaya.vercel.app',
+  //     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+  //     credentials: true,  // Permite todas las solicitudes de origen
+  // });
 
   await app.listen(3001);
 }
--- back/src/modules/users/users.module.ts
+++ back/src/modules/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
@@ -9,10 +9,21 @@ import { PropertyRepository } from '../property/property.repository';
 import { Property } from 'src/entities/property.entity';
 import { EmailRepository } from '../email/email.repository';
 import { Email } from 'src/entities/email.entity';
+import { requiresAuth } from 'express-openid-connect';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User,Property,Email])],
+  imports: [TypeOrmModule.forFeature([User, Property, Email])],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository,EmailService,PropertyRepository,EmailRepository],
+  providers: [
+    UsersService,
+    UsersRepository,
+    EmailService,
+    PropertyRepository,
+    EmailRepository,
+  ],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(requiresAuth()).forRoutes('users/auth0/protected');
+  }
+}
